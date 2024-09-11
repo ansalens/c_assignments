9b01e20ba9395baf9236054ea63e813baf7725ff
Sale <777aleksandarnikolic@gmail.com>
2024-09-11 09:53:55 +0200

    Complete assignment #2

---
+++ assignment-2/printvalues.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+    int num = 23;
+    float num2 = 0.789;
+    char string[] = "Hello World!";
+
+    printf("%d is an integer\n%0.3f is a float\n%s is a string\n", num, num2, string);
+
+    return 0;
+}
